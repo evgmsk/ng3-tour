888599d6d8c0259307aafe9d7b9b4b02b79fbf67
evgmsk <emoskvich@yandex.ru>
2021-01-27 01:03:41 +0300

    before update

--- projects/ng3-tour/src/lib/components/tour-step.component.spec.ts
+++ projects/ng3-tour/src/lib/components/tour-step.component.spec.ts
@@ -1,49 +1,48 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import {BehaviorSubject} from 'rxjs';
-import {Router, RouterModule} from '@angular/router';
-import {PLATFORM_ID, Inject} from '@angular/core';
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import {BehaviorSubject} from 'rxjs';
+// import {Router, RouterModule} from '@angular/router';
+// import {PLATFORM_ID, Inject} from '@angular/core';
 
 
-import {
-  TourService,
-  TourStepComponent,
-  StepTargetService,
-  AngularTourModule
-} from '../../public_api';
+// import {
+//   TourService,
+//   TourStepComponent,
+//   AngularTourModule
+// } from '../../public_api';
 
-describe('TourStepComponent', () => {
-  let component: TourStepComponent;
-  let fixture: ComponentFixture<TourStepComponent>;
-  let tour: TourService = null;
-  let router: Router;
-  let target: StepTargetService;
-  const stepsStream$ = new BehaviorSubject<any>('first');
-  const dummyApp = document.createElement('div')
-  dummyApp.setAttribute('ngTourStep', 'first');
+// // describe('TourStepComponent', () => {
+// //   let component: TourStepComponent;
+// //   let fixture: ComponentFixture<TourStepComponent>;
+// //   let tour: TourService = null;
+// //   let router: Router;
+// //   let target: StepTargetService;
+// //   const stepsStream$ = new BehaviorSubject<any>('first');
+// //   const dummyApp = document.createElement('div')
+// //   dummyApp.setAttribute('ngTourStep', 'first');
 
-  beforeEach(async(() => {
-    tour = new TourService(router, target, PLATFORM_ID);
-    target = new StepTargetService();
-    // console.log('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', Element);
-    TestBed.configureTestingModule({
-      imports: [AngularTourModule, RouterModule],
-      // declarations: [TourStepComponent],
-      providers: [{provide: TourService, useValue: tour }, {provide: StepTargetService, useValue: target }],
-      schemas: []
-    }).compileComponents();
-  }));
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TourStepComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+// //   beforeEach(async(() => {
+// //     tour = new TourService(router, target, PLATFORM_ID);
+// //     target = new StepTargetService();
+// //     // console.log('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', Element);
+// //     TestBed.configureTestingModule({
+// //       imports: [AngularTourModule, RouterModule],
+// //       // declarations: [TourStepComponent],
+// //       providers: [{provide: TourService, useValue: tour }, {provide: StepTargetService, useValue: target }],
+// //       schemas: []
+// //     }).compileComponents();
+// //   }));
+// //   beforeEach(() => {
+// //     fixture = TestBed.createComponent(TourStepComponent);
+// //     component = fixture.componentInstance;
+// //     fixture.detectChanges();
+// //   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-    const userService = fixture.debugElement.injector.get(TourService);
-    const usService = TestBed.get(TourService);
-  });
-  it ('simple test', () => {
-    expect(true).toBe(true)
-  })
-});
+// //   it('should create', () => {
+// //     expect(component).toBeTruthy();
+// //     const userService = fixture.debugElement.injector.get(TourService);
+// //     const usService = TestBed.get(TourService);
+// //   });
+// //   it ('simple test', () => {
+// //     expect(true).toBe(true)
+// //   })
+// // });
--- projects/ng3-tour/src/lib/services/tour.service.spec.ts
+++ projects/ng3-tour/src/lib/services/tour.service.spec.ts
@@ -1,16 +1,64 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
-import { TourService } from './tour.service';
+// import { TourService } from './tour.service';
+// import {Options} from '../interfaces/step.interface';
+// import {TourStep} from '../interfaces/tour.interface'
 
-describe('TourService', () => {
-  let service: TourService;
+// const steps: TourStep[] = [
+//     {
+//       stepName: 'first',
+//       route: 'home',
+//       title: 'Your tour started',
+//       description: 'Almost default settings. Only "top" placement is setted.',
+//     //   options: {withoutCounter: true, backdrop: false}
+//     },
+//     {
+//       stepName: 'second',
+//       route: 'courses',
+//       title: 'Courses Page',
+//       description: 'Lazily loaded',
+//       adds: 'Some adds',
+//     //   options: {customTemplate: true, smoothScroll: true, themeColor: '#254689', opacity: .9}
+//     },
+//     {
+//       stepName: 'third',
+//       route: 'courses/WAUQDI',
+//       title: {
+//         'en-EN': 'My first feature',
+//         'ru-RU': 'Моя первая фича',
+//         'fr-FR': 'Mon premier long métrage',
+//     },
+//     //   options: {customTemplate: true, placement: 'down', stepTargetResize: [5] }
+//     },
+//     {stepName: 'fourth', route: 'home', customData: 'Custom Data'},
+//     // {stepName: 'fifth', route: 'home', options: { placement: 'center', smoothScroll: true, stepTargetResize: [5], fixed: true }},
+//   ];
+// //   const tourOptions: Options = {
+// //     placement: 'top',
+// //     opacity: 0.7,
+// //     delay: 400,
+// //     // customTemplate: false,
+// //   };
+// //   const tourEvents: TourEvents = {
+// //     tourStart: ({tour}) => console.log(tour),
+// //     next: ({step, history}) => console.log(step, history),
+// //     prev: ({step}) => console.log(step),
+// //     tourBreak: ({step}) => console.log(step),
+// //     tourEnd: ({step}) => console.log(step),
+// //   };
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TourService);
-  });
+// describe('TourService', () => {
+//   let service: TourService;
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(TourService);
+//   });
+
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+//   it('should create tour', () => {
+
+//   })
+// });
